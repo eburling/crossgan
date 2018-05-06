0263bf2b23eba295f98ad3a6ddcd150dbea77b45
schaugf <schau.geoffrey@gmail.com>
2018-05-06 08:45:52 -0700

    init commit

---
+++ main.py
@@ -0,0 +1,46 @@
+""" Cross-domain learning model (WIP)
+"""
+
+import argparse
+import os
+
+from models import PhenoVAE
+
+parser = argparse.ArgumentParser(description='')
+
+parser.add_argument('--data_dir',       type=str,   default='data',     help='input data directory')
+parser.add_argument('--results_dir',    type=str,   default='results',  help='save directory')
+parser.add_argument('--phase',          type=str,   default='train',    help='train or load')
+
+parser.add_argument('--image_size',     type=int,   default=64,         help='image size')
+parser.add_argument('--image_channel',  type=int,   default=3,          help='image channels')
+
+parser.add_argument('--latent_dim',     type=int,   default=2,          help='latent dimension')
+parser.add_argument('--inter_dim',      type=int,   default=64,         help='intermediate dimension')
+parser.add_argument('--num_conv',       type=int,   default=3,          help='number of convolutions')
+parser.add_argument('--batch_size',     type=int,   default=32,         help='batch size')
+parser.add_argument('--epochs',         type=int,   default=2,          help='training epochs')
+parser.add_argument('--nfilters',       type=int,   default=64,         help='num convolution filters')
+parser.add_argument('--learn_rate',     type=float, default=0.001,      help='learning rate')
+parser.add_argument('--epsilon_std',    type=float, default=1.0,        help='epsilon width')
+
+args = parser.parse_args()
+
+
+def main():
+
+    os.makedirs(args.results_dir, exist_ok=True)
+        
+    if args.phase == 'train':
+        model = PhenoVAE(args)
+        model.train()
+
+    if args.phase == 'load':
+        from keras.models import load_model
+        model = load_model(os.path.join(args.results_dir, 'vae_model.h5'))
+        
+    
+
+# run main function
+if __name__ == '__main__':
+    main()
---
+++ models.py
@@ -0,0 +1,210 @@
+import os
+import glob 
+from PIL import Image
+import numpy as np
+        
+from keras.layers import Input, Conv2D, Flatten, Dense, Reshape, Lambda, Conv2DTranspose
+from keras import optimizers
+from keras import metrics
+from keras.models import Model
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+
+
+class PhenoVAE():
+    """ 2-dimensional variational autoencoder
+    """
+    
+    def __init__(self, args):
+        """ Initialize model with argument parameters and build
+        """
+
+        self.data_dir       = args.data_dir
+        self.save_dir       = args.results_dir
+        self.image_size     = args.image_size
+        self.image_channel  = args.image_channel
+        self.latent_dim     = args.latent_dim
+        self.inter_dim      = args.inter_dim
+        self.num_conv       = args.num_conv
+        self.batch_size     = args.batch_size
+        self.epochs         = args.epochs
+        self.nfilters       = args.nfilters
+        self.learn_rate     = args.learn_rate
+        self.epsilon_std    = args.epsilon_std
+        
+        self.phase          = args.phase
+        
+        self.build_model()
+        self.build_datagen()
+    
+    
+    def sampling(self, sample_args):
+        """ sample latent layer from normal prior
+        """
+        
+        z_mean, z_log_var = sample_args
+        
+        epsilon = K.random_normal(shape=(K.shape(z_mean)[0], self.latent_dim),
+                                  mean=0,
+                                  stddev=self.epsilon_std)
+    
+        return z_mean + K.exp(z_log_var) * epsilon
+    
+    
+    def build_model(self):
+        """ Build VAE model
+        """
+        
+        input_dim = (self.image_size, self.image_size, self.image_channel)
+        
+        #   encoder architecture
+        
+        x = Input(shape=input_dim)
+        
+        conv_1 = Conv2D(self.image_channel,
+                        kernel_size=(2, 2),
+                        padding='same', activation='relu')(x)
+        
+        conv_2 = Conv2D(self.nfilters,
+                        kernel_size=(2, 2),
+                        padding='same', activation='relu',
+                        strides=(2, 2))(conv_1)
+        
+        conv_3 = Conv2D(self.nfilters,
+                        kernel_size=self.num_conv,
+                        padding='same', activation='relu',
+                        strides=1)(conv_2)
+        
+        conv_4 = Conv2D(self.nfilters,
+                        kernel_size=self.num_conv,
+                        padding='same', activation='relu',
+                        strides=1)(conv_3)
+        
+        flat = Flatten()(conv_4)
+        hidden = Dense(self.inter_dim, activation='relu')(flat)
+        
+        #   reparameterization trick
+        
+        z_mean = Dense(self.latent_dim)(hidden)        
+        z_log_var = Dense(self.latent_dim)(hidden)
+        
+        z = Lambda(self.sampling)([z_mean, z_log_var])
+        
+        
+        #   decoder architecture
+
+        output_dim = (self.batch_size, self.image_size//2, self.image_size//2, self.nfilters)
+        
+        #   instantiate rather than pass through 
+        
+        decoder_hid = Dense(self.inter_dim, 
+                            activation='relu')
+        
+        decoder_upsample = Dense(self.nfilters * self.image_size//2 * self.image_size//2, 
+                                 activation='relu')
+
+        decoder_reshape = Reshape(output_dim[1:])
+        
+        decoder_deconv_1 = Conv2DTranspose(self.nfilters,
+                                           kernel_size=self.num_conv,
+                                           padding='same',
+                                           strides=1,
+                                           activation='relu')
+        
+        decoder_deconv_2 = Conv2DTranspose(self.nfilters,
+                                   kernel_size=self.num_conv,
+                                   padding='same',
+                                   strides=1,
+                                   activation='relu')   
+        
+        decoder_deconv_3_upsamp = Conv2DTranspose(self.nfilters,
+                                                  kernel_size=(3, 3),
+                                                  strides=(2, 2),
+                                                  padding='valid',
+                                                  activation='relu')
+        
+        decoder_mean_squash = Conv2D(self.image_channel,
+                                     kernel_size=2,
+                                     padding='valid',
+                                     activation='sigmoid')
+        
+        hid_decoded             = decoder_hid(z)
+        up_decoded              = decoder_upsample(hid_decoded)
+        reshape_decoded         = decoder_reshape(up_decoded)
+        deconv_1_decoded        = decoder_deconv_1(reshape_decoded)
+        deconv_2_decoded        = decoder_deconv_2(deconv_1_decoded)
+        x_decoded_relu          = decoder_deconv_3_upsamp(deconv_2_decoded)
+        x_decoded_mean_squash   = decoder_mean_squash(x_decoded_relu)
+
+        #   need to keep generator model separate so new inputs can be used
+        
+        decoder_input = Input(shape=(self.latent_dim,))
+        _hid_decoded = decoder_hid(decoder_input)
+        _up_decoded = decoder_upsample(_hid_decoded)
+        _reshape_decoded = decoder_reshape(_up_decoded)
+        _deconv_1_decoded = decoder_deconv_1(_reshape_decoded)
+        _deconv_2_decoded = decoder_deconv_2(_deconv_1_decoded)
+        _x_decoded_relu = decoder_deconv_3_upsamp(_deconv_2_decoded)
+        _x_decoded_mean_squash = decoder_mean_squash(_x_decoded_relu)
+        
+        
+        #   instantiate VAE models
+        
+        self.vae = Model(x, x_decoded_mean_squash)
+        self.encoder = Model(x, z_mean)
+        self.decoder = Model(decoder_input, _x_decoded_mean_squash)
+        
+        
+        #   VAE loss terms w/ KL divergence
+        
+        xent_loss = self.image_size * self.image_size * metrics.binary_crossentropy(K.flatten(x), 
+                                                                                    K.flatten(x_decoded_mean_squash))
+        
+        kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+
+        vae_loss = K.mean(xent_loss + kl_loss)
+        
+        self.vae.add_loss(vae_loss)
+        
+        rms = optimizers.adagrad(lr=self.learn_rate)
+        
+        self.vae.compile(optimizer=rms)
+        
+        self.vae.summary()
+            
+    
+    def build_datagen(self):
+        """ data generator for VAE
+        """
+
+        imageList=glob.glob(os.path.join(self.data_dir, '*.png'))
+        
+        self.loadedImages = np.array([np.array(Image.open(fname)) for fname in imageList])
+        
+        self.datagen = ImageDataGenerator(rescale=1. / 255,
+                                     horizontal_flip = True,
+                                     vertical_flip = True)
+        
+        self.datagen.fit(self.loadedImages)
+        
+        
+    def train(self):
+        """ train VAE model
+        """
+        
+        self.vae.fit(self.loadedImages,
+                     epochs = self.epochs,
+                     batch_size = self.batch_size)
+        
+        self.vae.save(os.path.join(self.save_dir, 'vae_model.h5'))
+        
+#        self.vae.fit_generator(self.datagen.flow(self.loadedImages,
+#                                                 None,
+#                                                 batch_size = self.batch_size),
+#                               shuffle=True,
+#                               epochs=self.epochs,
+#                               steps_per_epoch=100)
+#    
+        
+        
+        
